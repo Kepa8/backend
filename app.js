d184bf56de41ebdc1761655dfce45011c5526ece
Kepa8 <144702588+Kepa8@users.noreply.github.com>
2023-10-25 12:08:20 +0000

    g2

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
-let express = require('express');
+app.post("/bezeroa", function (req, res) {
+    // datuak eskuratu req.body-tik
+    let izena = req.body.izena;
+    let email = req.body.email;
+    let password = req.body.password;
+    let confpassword = req.body.confpassword;
+    let gender = req.body.gender;
+    let data = req.body.date
+    let address = req.body.address;
+    let city = req.body.city;
+    let zip = req.body.zip;
+    let agree = req.body.agree;
+    let newsletter = req.body.newsletter;
 
-let app = express();
-    app.use(express.urlencoded({extended: false}));
-    app.use(express.static('public'))
-    app.get("/", function(req, res) {
-    res.send("Kaixo mundua!");
-    });
 
-app.listen( 3000, function() {
-console.log("Zerbitzaria 3000 portuan entzuten");
-})
+    // Datuekin JSON bat sortu
+    let datuakJSON = {
+        "izena": izena,
+        "email": email,
+        "password" : password,
+        "confirm-password": confpassword,
+        "gender": gender,
+        "dob" : data,
+        "address": address,
+        "city": city,
+        "zip": zip,
+        "agree": agree,
+        "newsletter": newsletter
 
-app.post("/bezeroa",function(req,res){
+    };
 
-    res.send("Bezero izena:"+ $(req.body.izena) + " " + $(req.body.abizena));
+    // Bidali erantzun bat bezeroari datuekin
+    res.json(datuakJSON);
 });
-
-
-app.get('/bezero',function(req, res){
-    res.send(`Kaixo ${req.body.izena} ${req.query.abizena}`);
-});
-//app.post("/bezeroa",function(req,res){
-
-   // res.send("POST eskaera jasu dut");
-//});
